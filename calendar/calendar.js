0b93702839c51c5e4e7bcfb77a9b48d94218aa99
Roan Vickerman <29734170+14ROVI@users.noreply.github.com>
2021-07-17 23:23:59 +0100

    added contrast colour for light colours

--- calendar/calendar.js
+++ calendar/calendar.js
@@ -90,6 +90,15 @@ function loadCalendarPage(month, year) {
         el.innerText = i + 1
         calendar.appendChild(el)
     }
+
+    children = Array.prototype.slice.call(calendar.children).splice(7, 1000)
+    children.forEach(child => {
+        if (isColourLight(child.style.backgroundColor)) {
+            child.style.color = "black"
+        }
+    })
+
+
     loadClickHandlers()
 }
 
@@ -133,4 +142,48 @@ function trimChar(string, charToRemove) {
     }
 
     return string;
+}
+
+
+
+function isColourLight(color) {
+
+    // Variables for red, green, blue values
+    var r, g, b, hsp;
+    
+    // Check the format of the color, HEX or RGB?
+    if (color.match(/^rgb/)) {
+
+        // If RGB --> store the red, green, blue values in separate variables
+        color = color.match(/^rgba?\((\d+),\s*(\d+),\s*(\d+)(?:,\s*(\d+(?:\.\d+)?))?\)$/);
+        
+        r = color[1];
+        g = color[2];
+        b = color[3];
+    } 
+    else {
+        
+        // If hex --> Convert it to RGB: http://gist.github.com/983661
+        color = +("0x" + color.slice(1).replace( 
+        color.length < 5 && /./g, '$&$&'));
+
+        r = color >> 16;
+        g = color >> 8 & 255;
+        b = color & 255;
+    }
+    
+    // HSP (Highly Sensitive Poo) equation from http://alienryderflex.com/hsp.html
+    hsp = Math.sqrt(
+    0.299 * (r * r) +
+    0.587 * (g * g) +
+    0.114 * (b * b)
+    );
+
+    // Using the HSP value, determine whether the color is light or dark
+    if (hsp>127.5) {
+        return true;//light
+    } 
+    else {
+        return false;//dark
+    }
 }
