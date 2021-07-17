46c2e6ff1d09f9947dcdf6b2736753dc05fabc24
Sank6 <25014241+Sank6@users.noreply.github.com>
2021-07-17 17:19:29 +0100

    test*

--- points.js
+++ points.js
@@ -7,3 +7,15 @@ function toggleActivity(self, points){
         pointsCounter.innerText = parseInt(pointsCounter.innerText) - points
     }
 }
+
+function edit(id) {
+    let label = findLableForControl(document.getElementById(id));
+    label.contentEditable == "true" ? label.contentEditable == "false" : label.contentEditable = "true";
+}
+
+function findLableForControl(el) {
+    labels = document.getElementsByTagName('label');
+    for(let i = 0; i < labels.length; i++ ) {
+        if (labels[i].htmlFor == el.id) return labels[i];
+    }
+}
